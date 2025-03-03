be4027dae32093b709e03eeb99c3329c5665f58d
SaparbayevSarvarbek <sarvar3874@gmail.com>
2025-03-03 09:46:31 +0500

    upload home page

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -8,10 +8,59 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _serachContorller=TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Scaffold(),
+      appBar: AppBar(
+        title: Text('E-SHOP'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _serachContorller,
+              decoration: InputDecoration(
+                hintText: 'Search for tshirts'
+              ),
+            )
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Color(0xFFFC6828)),
+              child: Text('Drawer header'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Setting"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text("Info"),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
